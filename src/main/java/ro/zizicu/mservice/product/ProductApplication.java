54099e2f2c9f401b36d55c58ec62bea39aefcf4c
mateifl <mateifl@yahoo.com>
2017-10-06 17:22:12 +0300

    .

--- src/main/java/ro/zizicu/mservice/product/ProductApplication.java
+++ src/main/java/ro/zizicu/mservice/product/ProductApplication.java
@@ -2,12 +2,8 @@ package ro.zizicu.mservice.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-
-import ro.zizicu.mservice.product.controller.ProductController;
 
 @SpringBootApplication
-//@ComponentScan(basePackageClasses = ProductController.class)
 public class ProductApplication {
 
 	public static void main(String[] args) {
