cc9d30fa0573bbbc93851652169d6a1f6bc9fb32
Matei Florescu <matei@yahoo.com>
2024-02-12 12:56:19 +0200

    Added DTOs

--- src/main/java/ro/zizicu/mservice/product/services/ProductService.java
+++ src/main/java/ro/zizicu/mservice/product/services/ProductService.java
@@ -2,19 +2,10 @@ package ro.zizicu.mservice.product.services;
 
 import java.util.List;
 
-import ro.zizicu.mservice.product.data.ProductRepository;
 import ro.zizicu.mservice.product.dto.ProductDto;
-import ro.zizicu.mservice.product.dto.ProductDtoConverter;
-import ro.zizicu.mservice.product.entities.Product;
-import ro.zizicu.nwbase.service.LoaderService;
-import ro.zizicu.nwbase.service.NamedService;
-import ro.zizicu.nwbase.service.impl.DefaultLoaderService;
+import ro.zizicu.nwbase.service.DTOService;
 
-public interface ProductService extends LoaderService<ProductRepository,
-													  ProductDtoConverter,
-													  Product,
-													  ProductDto,
-													  Integer> {
+public interface ProductService extends DTOService<ProductDto, Integer> {
 
 	/** Execute a full or partial update on the product */
 	ProductDto update(ProductDto p);
